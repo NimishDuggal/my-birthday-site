72fbf7344517bd88c8f1d5b97a18bcaba87fbaa1
NimishDuggal <169060124+NimishDuggal@users.noreply.github.com>
2025-12-20 05:22:35 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -171,7 +171,7 @@ function App() {
         <Gallery isActive={currentPage === 4} />
         <section className="final">
           <h2 className="final-message">💖 Yours Only — Nimish 💖</h2>
-          <p className="final-subtitle"> Couldn't join you for your bithday but hoping to see you very soonnnnn✨</p>
+          <p className="final-subtitle"> Fas Fas milne ka plann banaooo :) </p>
         </section>
       </div>
 
